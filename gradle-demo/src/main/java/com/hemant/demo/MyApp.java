60df58cf61e0287a476aef05aa71434c2e44073d
Hemant <hemantmali21@gmail.com>
2017-05-18 23:22:01 +0530

    This demo explains springboot and gradle
    spring-boot-gradle

---
+++ gradle-demo/src/main/java/com/hemant/demo/MyApp.java
@@ -0,0 +1,15 @@
+package com.hemant.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        SpringApplication.run(MyApp.class, args);
+		
+	}
+
+}
